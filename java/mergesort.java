6918888ce6583706343cffbfb80a98805020635b
Dimitar Panayotov <neznajko@yahoo.com>
2022-03-09 11:43:48 +0200

    yeah

--- java/mergesort.java
+++ java/mergesort.java
@@ -34,37 +34,37 @@ import java.util.*;
 //////////////////////////////////////////////////// Driver Code
 class Merge_Sort {
     //method to print the elements of the array
-	static void printArray( int arr[]) {
+    static void printArray( int arr[]) {
         int n = arr.length;
         for( int i = 0; i < n; ++i) {
             System.out.print( arr[i] + " ");
         }
         System.out.println();
     }
-	public static void main( String args[]) {
-	    //taking input using Scanner class
-		Scanner sc = new Scanner( System.in);
-		//taking testcases
-		int T = sc.nextInt();
-		while( T > 0) {
-		    //taking elements count
-			int n = sc.nextInt();
-			//creating an object of class Merge_Sort
-			Merge_Sort ms = new Merge_Sort();
-			//creating an array of size n
-			int arr[] = new int[ n];
-			//adding elements to the array
-			for( int i = 0; i < n; i++) {
-				arr[ i] = sc.nextInt();
+    public static void main( String args[]) {
+        //taking input using Scanner class
+        Scanner sc = new Scanner( System.in);
+        //taking testcases
+        int T = sc.nextInt();
+        while( T > 0) {
+            //taking elements count
+            int n = sc.nextInt();
+            //creating an object of class Merge_Sort
+            Merge_Sort ms = new Merge_Sort();
+            //creating an array of size n
+            int arr[] = new int[ n];
+            //adding elements to the array
+            for( int i = 0; i < n; i++) {
+                arr[ i] = sc.nextInt();
             }            
-			Solution g = new Solution();			
-			//calling the method mergeSort
-			g.mergeSort( arr, 0, arr.length - 1);
+            Solution g = new Solution();            
+            //calling the method mergeSort
+            g.mergeSort( arr, 0, arr.length - 1);
             //calling the method printArray
-			ms.printArray( arr);
+            ms.printArray( arr);
             T--;
-		}
-	}
+        }
+    }
 }
 ////////////////////////////////////////////////////////////////
 class Solution {
